3695e820b50ac18dddb62c046bf4267d2382e689
Hong-She Liang <starofrainnight@gmail.com>
2015-10-11 17:53:09 +0800

    Added basic setup framework files

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from rabird_bootstrap import use_pip
+use_pip()
+
+import glob
+from setuptools import setup, find_packages
+
+package_name = "u8glib_helper"
+
+# Exclude the original source package, only accept the preprocessed package!
+our_packages = find_packages()
+    
+our_requires = [
+    "six>=1.3.0"
+    ]
+    
+long_description=(
+     open("README.rst", "r").read()
+     + "\n" +
+     open("CHANGES.rst", "r").read()
+     )
+
+our_scripts = glob.glob("scripts/*")
+
+setup(
+    name=package_name,
+    version="0.0.1",
+    author="Hong-She Liang",
+    author_email="starofrainnight@gmail.com",
+    url="https://github.com/starofrainnight/%s" % package_name,
+    description="A series helper scripts for u8glib",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",        
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent", 
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: Implementation :: CPython",        
+        "Topic :: Software Development :: Libraries",
+    ],
+    install_requires=our_requires,
+    packages=our_packages,
+    scripts=our_scripts,
+    )
